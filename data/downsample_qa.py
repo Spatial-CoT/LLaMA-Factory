1782458eb021432b3f063ab933930e1ed1c3906b
Anjali Gupta <anjaligupta1104@gmail.com>
2025-02-06 22:02:22 +0000

    ft scripts

--- data/downsample_qa.py
+++ data/downsample_qa.py
@@ -2,14 +2,14 @@ import json
 import random
 
 # Read the original JSON file
-with open('data/all_qa_sharegpt_0203.json', 'r') as f:
+with open('/home/anjali/LLaMA-Factory/data/all_qa_pairs_spatial_llama-factory-20250204.json', 'r') as f:
     data = json.load(f)
 
 # Randomly sample 25% of entries
 sampled_data = random.sample(data, k=int(len(data) * 0.25))
 
 # Write sampled data to new JSON file
-output_file = 'data/all_qa_sharegpt_0203_25percent.json'
+output_file = 'data/all_qa_spatial_0204_25percent.json'
 with open(output_file, 'w') as f:
     json.dump(sampled_data, f, indent=2)
 
